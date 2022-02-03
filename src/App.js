b36ca04b51d27e20165de3c3e2dc500dcbea116e
AseelAlAseel <aseel1234500000@gmail.com>
2022-02-03 12:50:16 +0200

    add user List and add function and some design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import "./App.scss"
-import Form from './components/InputForm/Form';
-const App=()=>{
-    return(
-        <div>
-            <Form/>
-        </div>
-    )
-}
+import React, { useState } from "react";
+import "./App.scss";
+import Form from "./components/InputForm/Form";
+import PrintUsersList from "./components/PrintUserList/PrintUsersList";
+const usersList = [
+  {
+    name: "Ahgmad",
+    age: 30,
+  },
+];
+const App = () => {
+  const [List, setList] = useState(usersList);
+
+  const AddNewUser = (newUser) => {
+    setList((prevUserList) => [...prevUserList, newUser]);
+  };
+  return (
+    <div>
+      <Form onAddNewUser={AddNewUser} />
+
+      <PrintUsersList usersList={List} />
+    </div>
+  );
+};
 
 export default App;

2d102021ff94a25b05528b550c896b7708fb5b66
AseelAlAseel <aseel1234500000@gmail.com>
2022-02-03 20:02:27 +0200

    move  testing lines

--- src/components/PrintUserList/PrintUsersList.js
+++ src/components/PrintUserList/PrintUsersList.js
@@ -2,13 +2,13 @@ import React from "react";
 import Card from "../UI/Card";
 import "./PrintUsersList.scss"
 const PrintUsersList = (props) => {
-    {console.log(props.usersList)}
+
   const Print = props.usersList.map((user, index) => {
    return <div className="printUser" key={index}>
       {user.name} ({user.age} years old)
     </div>;
   });
-  console.log(Print)
+
 
   return <Card className="print-users-control">{Print}</Card>;
 };

1098b9a65a288728107d6875eb94265b187c3bbf
AseelAlAseel <aseel1234500000@gmail.com>
2022-02-04 15:21:14 +0200

    change the click of panel

--- src/components/PrintError/PrintError.js
+++ src/components/PrintError/PrintError.js
@@ -2,24 +2,20 @@ import React from "react";
 import Card from "../UI/Card";
 import "./PrintError.scss";
 const PrintError = (props) => {
-
-  document.addEventListener("click",()=>{
+  const okayHandler = () => {
     props.onOkay();
-  })
-  const okayHandler=()=>{
-    props.onOkay();
-  }
+  };
   return (
-    <div className="error-panel">
+    <div className="error-panel" onClick={okayHandler}>
       <Card className="error-control">
         <div className="erroe-head">
           <h1>Invalid input</h1>
         </div>
         <div className="error-type">
-            <p>{props.text}</p>
+          <p>{props.text}</p>
         </div>
         <div className="okay">
-            <button onClick={okayHandler}>Okay</button>
+          <button onClick={okayHandler}>Okay</button>
         </div>
       </Card>
     </div>
